a3d1bd1cec87a6588939625fde1fd936b8539ae6
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2024-02-10 10:43:23 +0100

    Prepare for 0.05 version

--- DwgRead.pm
+++ DwgRead.pm
@@ -16,7 +16,7 @@ use Readonly;
 
 Readonly::Scalar our $DR => 'dwgread';
 
-our $VERSION = 0.04;
+our $VERSION = 0.05;
 
 # Constructor.
 sub new {
@@ -255,6 +255,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.04
+0.05
 
 =cut
